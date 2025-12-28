a1dcee29d5cf49c6fbc1baf71c5dc5382d8b57a7
Gautam-College <gautamdoliya69@gmail.com>
2025-12-28 21:14:58 +0530

    Api

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,6 +8,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "https://five-furniture.onrender.com",
+        "https://furniture.gautamdoliya69.workers.dev"
     ],
 }))
 app.use(cookieParser())
